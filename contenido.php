d790393968aa7445aab43c8d5a59fd5e50b3baa6
jair71021 <DELL>
2021-03-25 09:03:57 -0600

    Ejercicio de imagen estilo coon google en php

---
+++ contenido.php
@@ -0,0 +1,79 @@
+<?php
+
+function contenido(){
+  $datos=array();
+
+  $datos[0]="img/1.jpg"."||".
+            "Cabinho (1974-1987) "."||".
+            "Evanivaldo Castro Silva, comúnmente conocido por su sobrenombre Cabinho, es un exfutbolista brasileño 
+            que se desempeñó como delantero; desarrolló la mejor parte de su carrera en México. Es el máximo anotador
+             de la Primera División de México, anotando 312 goles y consiguiendo ocho títulos de goleo, cifra que también
+            significa un récord,jugó entre 1974 y 1988 para los equipos de la UNAM, Atlante, León y Tigres. Es considerado 
+            como uno de los mejores extranjeros que han llegado al fútbol de México.";
+
+
+  $datos[1]="img/2.jpg"."||".
+            "Carlo Hermosillo (1984-2001) "."||".
+            "Carlos Manuel Hermosillo Goytortúa es un exfutbolista mexicano que jugaba como delantero. 
+            Es el segundo máximo goleador en la historia de la Primera División de México (solo por debajo de Evanivaldo Castro Cabinho), 
+            y el 5° máximo anotador de la Selección de fútbol de México, 
+            solo detrás de Javier Hernández, Jared Borgetti, Cuauhtémoc Blanco y Luis Hernández. 
+            En su desempeñó como centro delantero, la característica más notoria fue su contundente remate de cabeza. ";
+
+
+  $datos[2]="img/3.jpg"."||".
+            "Jared Borgetti(1994-2005 y 2007-2010)"."||".
+            " Jared Francisco Borgetti Echavarría es un exfutbolista mexicano​ que jugó en la posición de delantero durante la década de 1990 
+            y la década de 2000. Borgetti se caracterizó por su preciso remate de cabeza y su gran visión de campo. ";
+
+
+  $datos[3]="img/4.jpg"."||".
+            "José Saturnino Cardozo Otazú (1994-2005)"."||".
+            "José Saturnino Cardozo Otazú es un exjugador profesional y actual entrenador de fútbol paraguayo naturalizado mexicano, se desempeñaba 
+            como delantero.Con el club que se le ha identificado más es con el Deportivo Toluca F. C. de la Primera División de México, con el que 
+            jugó desde la temporada 1994/1995 hasta el 2005,ganando con el mismo cuatro ligas e igual número de títulos de goleo. Vistiendo 
+            la camiseta de los Diablos Rojos convirtió 249 goles,convirtiéndose en el máximo goleador en la historia de la institución y en un gran 
+            delantero histórico para el fútbol mexicano";
+
+
+  $datos[4]="img/5.jpg"."||".
+            "Horacio Casarín Garcilazo(1936-1957)"."||".
+            "Horacio Casarín Garcilaz fue un futbolista y entrenador mexicano que se desempeñaba como delantero. Fue la máxima figura de la primera
+             mitad del siglo XX en el fútbol mexicano. Se retiró como máximo goleador de todos los tiempos en el Campeonato de Liga, con 238 goles   ";
+
+
+  $datos[5]="img/6.jpg"."||".
+            "Osvaldo Castro Pelayo(1971-1984) "."||".
+            "Osvaldo Castro Pelayomás conocido por su apodo, Pata Bendita, es un exfutbolista profesional chileno, que jugaba de delantero.
+             Castro era conocido por su potente disparo de pierna izquierda y su gran olfato goleador. ";
+
+
+  $datos[6]="img/7.jpg"."||".
+            "Luis Roberto Alves dos Santos Gavranic (1986-2003)"."||".
+            "Luis Roberto Alves más conocido como Zague o Zaguinho es un exfutbolista mexicano de ascendencia brasileña que jugaba como delantero. 
+            Jugó para los equipos América, Atlante y Necaxa en México y en Brasil para el Corinthians, donde perteneció a las categorías juveniles hasta los 17 años. ";
+
+
+  $datos[7]="img/8.jpg"."||".
+            "Adalberto López(1942-1955)"."||".
+            "Adalberto López popularmente conocido como Dumbo López, fue un futbolista mexicano que jugaba en la posición de delantero.
+             Jugó para el Atlante, León, Atlas, Oro y Guadalajara.Fue el primer jugador en superar la marca de 30 goles en una temporada
+              entre los mexicanos. En su carrera ganó 5 campeonatos de goleo, cantidad que permaneció como la más alta de la liga hasta 1981 
+              cuando Cabinho obtuvo el sexto de sus ocho títulos.";
+
+
+  $datos[8]="img/9.jpg"."||".
+            "Carlos Eloir Peruci (1972-84)"."||".
+            "Carlos Eloir Peruci es un exfutbolista brasileño.Vistió las camisetas de diversos clubs, entre los que se encuentran
+             los Club de Fútbol Laguna, el desaparecido Atlético Español y el Cruz Azul. Uno de los máximos goleadores que han venido a México.";
+
+
+  $datos[9]="img/10.jpg"."||".
+            "Sergio Lira Gallardo (1978-1996)"."||".
+            "Sergio Lira Gallardo es un exfutbolista y entrenador mexicano que jugaba en la posición de delantero. 
+            Surgió del Tampico y militó también para Tampico Madero, Atlante, Oaxtepec, UANL y Puebla FC. Fue campeón de goleo en tres ocasiones.";
+  return $datos;
+
+}
+
+?>
